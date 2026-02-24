0f013074346b3a19ec92a6b77d0c18600c2757cc
smocap <smocap@gmail.com>
2026-02-24 05:40:21 +0100

    Translation update (es)

---
+++ lang/es/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author smocap <smocap@gmail.com>
+ */
+$lang['db_name']               = 'Cadena de conexión a la base de datos (ejemplo: "mysql:host=localhost;dbname=repo")';
+$lang['db_user']               = 'Usuario de la base de datos';
+$lang['db_pass']               = 'Contraseña de la base de datos';
+$lang['bundled']               = 'Extensiones incluidas en la versión actual, separadas por comas';
+$lang['releases']              = 'Lista de versiones (aaaa-mm-dd "nombre"), separadas por comas';
+$lang['github_user']           = 'Usuario para autenticarse en GitHub y evitar las limitaciones de la API.';
+$lang['github_key']            = 'Clave API para el usuario anterior.';
